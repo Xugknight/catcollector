183f3208ebac4f669017de69481fe8c018d6c5f3
timothyn <tnguyen_42@yahoo.com>
2025-07-07 12:43:15 -0400

    Create Detail Page and Update URL

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Cat
 
 # Create your views here.
 def home(request):
@@ -10,4 +11,9 @@ def about(request):
 
 def cat_index(request):
     # Render the cats/index.html template with the cats data
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
