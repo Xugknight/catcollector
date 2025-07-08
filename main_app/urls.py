f91dd58af4a6616d72a611e3503b3a7a0dc8e840
timothyn <tnguyen_42@yahoo.com>
2025-07-08 11:00:21 -0400

    Add CatCreate

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    # route for cats index
     path('cats/', views.cat_index, name='cat-index'),
     path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
+    path('cats/create/', views.CatCreate.as_view(), name='cat-create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from .models import Cat
 
 # Create your views here.
 def home(request):
-    # return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def cat_index(request):
-    # Render the cats/index.html template with the cats data
     cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     return render(request, 'cats/detail.html', {'cat': cat})
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+    success_url = '/cats/'
