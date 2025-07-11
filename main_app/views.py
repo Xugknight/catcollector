2818ce05b58ba2fe596c489bb125393aa60b9c9d
timothyn <tnguyen_42@yahoo.com>
2025-07-11 12:03:54 -0400

    Finish sign up

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 import requests
 from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
@@ -97,3 +99,28 @@ def remove_toy(request, cat_id, toy_id):
     cat = Cat.objects.get(id__exact=cat_id)
     cat.toys.remove(toy_id)
     return redirect('cat-detail', cat_id=cat.id)
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in
+            login(request, user)
+            return redirect('cat-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error-message': error_message}
+    return render(request, 'signup.html', context)
+    # Same as: 
+    # return render(
+    #     request, 
+    #     'signup.html',
+    #     {'form': form, 'error_message': error_message}
+    # )
